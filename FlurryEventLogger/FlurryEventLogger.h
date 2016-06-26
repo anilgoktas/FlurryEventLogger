1d739ea3a81dfa4f42df81740240ff95690bdee9
Anıl Göktaş <goktas.anil@gmail.com>
2016-06-26 20:26:41 +0300

    Initial Commit

---
+++ FlurryEventLogger/FlurryEventLogger.h
@@ -0,0 +1,19 @@
+//
+//  FlurryEventLogger.h
+//  FlurryEventLogger
+//
+//  Created by Anıl Göktaş on 6/26/16.
+//  Copyright © 2016 Anıl Göktaş. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlurryEventLogger.
+FOUNDATION_EXPORT double FlurryEventLoggerVersionNumber;
+
+//! Project version string for FlurryEventLogger.
+FOUNDATION_EXPORT const unsigned char FlurryEventLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlurryEventLogger/PublicHeader.h>
+
+
